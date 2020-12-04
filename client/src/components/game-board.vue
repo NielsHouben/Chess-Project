d9e7463f5b9bab803eb69625f89a32e6a73ccfcd
Nisse2 <niels.houben1@gmail.com>
2020-12-05 00:56:41 +0100

    moved code for game-board to actually game-board component

--- client/src/components/game-board.vue
+++ client/src/components/game-board.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="board">
     <div class="gameBoard">
-      <div v-for="(piece, index) in board" :key="piece.id">
+      <div v-for="(piece, index) in state.board" :key="piece.id">
         <div class="square" v-bind:class="ColorDecider(index) ? 'whiteish' : 'green'">
           <div>
             <img v-if="piece" :src="require(`../assets/Pieces/${piece}.webp`)" />
@@ -14,14 +14,24 @@
 
 <script>
 import { reactive } from "vue";
+import { onMounted } from "vue";
+import { useRoute } from "vue-router";
+import axios from "axios";
+
+let sessionId;
 
 export default {
   name: "board",
-  props: {
-    board: Array,
-  },
-  setup(props) {
-    const state = reactive({ board: props.board });
+  // props: {
+  //   board: Array,
+  // },
+  setup() {
+    //props
+    const state = reactive({
+      // board: props.board,
+      board: [],
+      fetchGameBoard: true,
+    });
 
     //to generate checkered pattern,  problem was that every 8 it needed to be +1, basically, it chacks if its on an
     //odd or even row, if even: return true for all even squares
@@ -29,6 +39,31 @@ export default {
     function ColorDecider(index) {
       return Math.floor(index / 8) % 2 == 0 ? index % 2 == 0 : index % 2 != 0;
     }
+
+    //for fetching api -----------------------------------------------
+    onMounted(() => {
+      const route = useRoute();
+      sessionId = route.query.sessionId;
+      getBoard();
+    });
+
+    //code below basically makes an infinit loop (stoppable by making fetchGa... false)
+    //cant be a recursive function, would brake. has timer to stop the while loop for delay.
+    //requests the session from server, gets the session and buts its board into board variable.
+    const timer = (ms) => new Promise((res) => setTimeout(res, ms));
+
+    async function getBoard() {
+      // We need to wrap the loop into an async function for this to work
+      while (state.fetchGameBoard) {
+        const req = await axios.get(`/api/session/${sessionId}`, {});
+
+        // console.log(req.data.board);
+        state.board = req.data.board;
+
+        await timer(100); // then the created Promise can be awaited
+      }
+    }
+    //----------------------------------------------------------------
 
     return {
       state,
--- client/src/views/Game.vue
+++ client/src/views/Game.vue
@@ -1,51 +1,17 @@
 <template>
   <div>
-    <gameBoardComponent :board="state.board" id="game-board"></gameBoardComponent>
+    <gameBoardComponent id="game-board"></gameBoardComponent>
   </div>
 </template>
 
 <script>
 import { reactive } from "vue";
-import { onMounted } from "vue";
 import gameBoardComponent from "../components/game-board.vue";
-import { useRoute } from "vue-router";
-import axios from "axios";
-
-// async function getBoard() {
-//   const req = await axios.get(`/api/session/:${route.query.sessionId}`, {});
-//   return req;
-// }
-
-let sessionId;
 
 export default {
   name: "Home",
   setup() {
-    const state = reactive({
-      board: [],
-      fetchGameBoard: true,
-    });
-
-    onMounted(() => {
-      const route = useRoute();
-      sessionId = route.query.sessionId;
-      getBoard();
-    });
-
-    //code below basically makes an infinit loop (stoppable by making fetchGa... false)
-    //cant be a recursive function, would brake. has timer to stop the while loop for delay.
-    //requests the session from server, gets the session and buts its board into board variable.
-    const timer = (ms) => new Promise((res) => setTimeout(res, ms));
-
-    async function getBoard() {
-      // We need to wrap the loop into an async function for this to work
-      while (state.fetchGameBoard) {
-        const req = await axios.get(`/api/session/${sessionId}`, {});
-        state.board = req.data.board;
-
-        await timer(100); // then the created Promise can be awaited
-      }
-    }
+    const state = reactive({});
 
     return {
       state,
